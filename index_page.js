0008c40c50fe6008a956e0e5fbabb09f66a75d67
arturoservin <50938310+arturoservin@users.noreply.github.com>
2019-07-09 17:59:22 -0500

    js added

---
+++ index_page.js
@@ -0,0 +1,10 @@
+var boton = document.getElementById("more_portafolio");
+boton.addEventListener("click",alerta);
+
+var boton2 = document.getElementById("more_experiencia");
+boton2.addEventListener("click",alerta);
+
+function alerta ()
+{
+    alert("Estas seguro que deseas salir de esta pagina?");
+}
